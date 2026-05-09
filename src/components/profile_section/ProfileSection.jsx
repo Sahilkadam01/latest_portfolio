bc8fd10b1f90ec44c68a69859d79322155e6fb27
sahilkumar01 <sahilkadam2701@gmail.com>
2026-05-09 23:27:25 +0530

    commit 16 profile image error solve

--- src/components/profile_section/ProfileSection.jsx
+++ src/components/profile_section/ProfileSection.jsx
@@ -126,7 +126,7 @@ export default function ProfileSection() {
 
           {/* FLOATING IMAGE */}
           <motion.img
-            src="../src\assets\Sahil_kumar.png"
+            src="/public/assets/sahil_kumar_profile.png"
             alt="profile"
             animate={{ y: [0, -20, 0] }}
             transition={{ duration: 4, repeat: Infinity }}
